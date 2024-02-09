d0f7c993a323eabcea5d6b2c833c361e1ce9f5a2
Arik Kfir <arik@kfirs.com>
2024-02-09 21:59:57 +0200

    WIP

--- backend/e2e/init.go
+++ backend/e2e/init.go
@@ -4,6 +4,8 @@ import (
 	apiv1 "github.com/arikkfir/devbot/backend/api/v1"
 	"github.com/arikkfir/devbot/backend/internal/util/logging"
 	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega/format"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -19,4 +21,11 @@ func init() {
 	scheme = runtime.NewScheme()
 	utilruntime.Must(apiv1.AddToScheme(scheme))
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+
+	format.RegisterCustomFormatter(func(value interface{}) (string, bool) {
+		if t, ok := value.(metav1.Time); ok {
+			return t.String(), true
+		}
+		return "", false
+	})
 }
