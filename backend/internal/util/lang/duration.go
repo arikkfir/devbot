ba117c77ff5ed91785336fccc551b16c33aa90a3
Arik Kfir <arik@kfirs.com>
2024-02-09 22:00:37 +0200

    WIP

--- backend/internal/util/lang/duration.go
+++ backend/internal/util/lang/duration.go
@@ -5,11 +5,11 @@ import (
 	"time"
 )
 
-func ParseDuration(minSeconds int, value string) (time.Duration, error) {
+func ParseDuration(minDuration time.Duration, value string) (time.Duration, error) {
 	if duration, err := time.ParseDuration(value); err != nil {
 		return 0, err
-	} else if duration.Seconds() < float64(minSeconds) {
-		return 0, errors.New("refresh interval '%s' is too low (must not be less than %ss)", value, minSeconds)
+	} else if duration < minDuration {
+		return 0, errors.New("refresh interval '%s' is too low (must not be less than %s)", value, minDuration)
 	} else {
 		return duration, nil
 	}
