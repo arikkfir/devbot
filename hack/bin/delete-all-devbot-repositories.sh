77e56e1056f7e414c106b07e5d845c45efd7b5bf
Arik Kfir <arik@kfirs.com>
2024-02-09 16:50:32 +0200

    WIP

--- hack/bin/delete-all-devbot-repositories.sh
+++ hack/bin/delete-all-devbot-repositories.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# TODO: replace "op plugin run -- gh" with "gh" once 1password fixes the issue with the gh plugin
+
 gh repo list devbot-testing --json=nameWithOwner \
   | jq '.[]|.nameWithOwner' -r \
   | sort \
